acaaf2a78e2c1e96cffaa3429e2c1332dbd1502e
Peter Zošiak <peter.zosiak@external.t-mobile.cz>
2024-08-12 15:11:04 +0200

    prd prd

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@ import Services from '~/components/services.vue'
       <div class="ann-profile-info">
         <div class="d-flex align-items-center w-100">
           <div class="mx-auto ann-profile-inner">
-            <h2>Tina Poda<span>ESTHETIC</span></h2>
+            <h2 class="headline">Tina Poda<span>ESTHETIC</span></h2>
           </div>
         </div>
       </div>
@@ -26,7 +26,7 @@ import Services from '~/components/services.vue'
         <div class="col-md-8 offset-md-2 text-center">
           <div class="ann-about-info">
             <h1>About Me</h1>
-            <h3>Hi, I'm Ann a Professional Makeup Artist</h3>
+            <h3>Kosmetolog & Make-up Artist</h3>
             <p>Nulla molestie finibus dignissim. Nunc ultrices odio mauris fermentum, gravida varius dranex the celacinia. Proin dictum nisleu scelerisque risus feugiat the amet. Vestibulum condimentum sem non dui tempoeu. Aliquam non venenatis. Donec eget neque et nulla interdum iaculis seder.</p>
             <p class="ann-signature-dark"><img src="/img/signature.png" alt="Ann Martin"></p>
           </div>
--- pages/o-mne.vue
+++ pages/o-mne.vue
@@ -7,9 +7,14 @@
           <div class="col-md-4"> <img class="ann-about-img" src="/img/t1.jpg" alt="Tina Poda"> </div>
           <div class="col-md-8">
             <div class="ann-about-info">
-              <h2>About Me</h2>
-              <p>Hi, I'm Ann a Professional Makeup Artist. Vestibulum condimentum sem non dui tempoeu pharetra. Aliquam non venenatis. Donec eget neque et nulla interdum iaculis seder the gravida varius ex lacinia.</p>
-              <p>Nulla molestie finibus dignissim. Nunc ultrices odio eu mauris fermentum the gravida varius ex lacinia. Proin dictum nisl orci, eu scelerisque risus feugiat sit amet. Vestibulum condimentum sem non dui tempoeu pharetra. Aliquam the venenatis libero mi. Donec eget neque et nulla interdum iaculis seder.</p>
+              <h2>Kto je Tina Poda</h2>
+              <p>
+                Celý môj život je prepojený s krásou a starostlivosťou o pleť. <strong>Beauty oblasti</strong> sa venujem cez 20 rokov a postupne som vytvorila jedinečný prístup, kombinujúci rozsiahle skúsenosti, prvotriedne produkty a inovatívne techniky. </p>
+              <p>Kozmetické procedúry dopĺňam o <strong>autentické rituály</strong>, čo napomáha maximalizovať pôžitok z času venovaného sebe. S klientami zároveň pracujem <strong>komplexne</strong> – od konzultácie, cez individuálnu starostlivosť o pleť podľa stavu pokožky, potreby a sezónnosti až po motiváciu k budovaní ich vlastných (a správnych) návykov. </p>
+
+              <p>Kariéru som budovala v renomovaných kozmetických salónoch na Slovensku a v Česku. Podieľala som sa na veľkých projektoch, zaškoľovaní tímov a vytváraní nových konceptov starostlivosti o pleť. Získala som certifikáciu rôznych oblastí starostlivosti o pleť a vizážistiky. Roky som pôsobila ako make-up artistka: líčila slovensko-český showbiznis, modelky na súťažiach krásy, fashion weekoch a iných významných príležitostiach. </p>
+              <p><strong>Dnes sa starám o svojich ctených klientov v srdci Vinohradov v Prahe.</strong></p>
+              <p>Zároveň mi je cťou byť súčasťou vzdelávacej akadémie ALMAF BEAUTY a predávať expertné skúsenosti aj kolegyniam v branži. </p>
               <p class="ann-signature-dark"><img src="/img/signature.png" alt="Ann Martin"></p>
             </div>
           </div>
